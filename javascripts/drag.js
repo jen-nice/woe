a3c799fbd55e387b0b1d868de435e121e1d77cc4
jencol <jennice.colaco@gmail.com>
2015-09-18 21:45:30 -0400

    Update drag.js

--- javascripts/drag.js
+++ javascripts/drag.js
@@ -1,11 +1,23 @@
 
-var main = function(){
+/* var main = function(){
  //To drag the pictures
  $(".wallsaying").fadeOut(1000);
  $( "#draggable" ).draggable();
 };
 
+$(document).ready(main); */
+
+var main = function(){
+ //To drag the pictures
+ 
+ $( "#wallpictures" ).click(function() {
+  $( "#draggable" ).draggable();
+ });
+
+};
+
 $(document).ready(main);
+
 
 
 	  
