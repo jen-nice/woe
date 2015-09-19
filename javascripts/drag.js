532c15fba6e60611489c076277ca9fa69de8f413
jencol <jennice.colaco@gmail.com>
2015-09-18 21:15:50 -0400

    added draggable back

--- javascripts/drag.js
+++ javascripts/drag.js
@@ -2,7 +2,11 @@
 var main = function(){
  //To drag the pictures
  $(".wallsaying").fadeOut(1000);
- $(".dragme").draggable();
+ $( "#draggable" ).draggable();
 };
 
 $(document).ready(main);
+
+
+	  
+  
